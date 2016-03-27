eeffb6c81855cf6ee2007def570094d6a140c4a5
MichaelMa <Minima2014@iCloud.com>
2016-03-27 19:01:54 +0800

    Initial Commit

---
+++ Oil/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Oil
+//
+//  Created by 马琛骁 on 16/3/27.
+//  Copyright © 2016年 Michael Ma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
